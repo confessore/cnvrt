dcd73fb62979f7f8315a8abc0e30863845e3f26e
Steven Confessore <steven.confessore@gmail.com>
2019-01-10 21:29:49 -0500

    added utf8_to_String

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,14 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 
 /// converts '*const c_char' to '&str'
-///
 pub fn cchar_to_str(cchar: *const c_char) -> &'static str {
     let c_buf: *const c_char = cchar;
     let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
     c_str.to_str().unwrap()
+}
+
+/// converts '&[i8]' to 'String'
+pub fn utf8_to_string(bytes: &[i8]) -> String {
+        unsafe { CStr::from_ptr(bytes.as_ptr()).to_string_lossy().into_owned() }
 }
 
